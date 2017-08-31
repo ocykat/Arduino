f2d3eaff33668b0bbeaeaa8df7de95fa5da6c073
Nhat Nguyen <nhatnguyen.sg.vn@gmail.com>
2017-08-31 21:46:57 +0700

    31-08-17 21:45

---
+++ oop1/oop1.ino
@@ -0,0 +1,42 @@
+struct LedPin {
+    int pinNumber;
+    int state;
+    String type;
+
+    LedPin(int _pinNumber, String _type) {
+        pinNumber = _pinNumber;
+        type = _type;
+    }
+    ~LedPin();
+
+    void setMode() {
+        if (type == "in") {
+            pinMode(pinNumber, INPUT);
+        } 
+        else if (type == "out") {
+            pinMode(pinNumber, OUTPUT);
+        }
+        else if (type == "inpu") {
+            pinMode(pinNumber, INPUT_PULLUP);
+        }
+    }
+    
+    void turnOn() {
+        digitalWrite(pinNumber, HIGH);
+    }
+
+    void turnOff() {
+        digitalWrite(pinNumber, LOW);
+    }
+};
+
+void setup() {
+}
+
+void loop() {
+    static LedPin lp(10, "out");
+    lp.turnOn();
+    delay(300);
+    lp.turnOff();
+    delay(300);
+}
